e72c323420d9af2f45a128463a593a1522dafe2c
shadowclan-R <rayanabdulazizm.ali@gmail.com>
2023-09-08 18:57:47 -0700

    updated

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+  * main - Prints the numbers since 0 to 9
+  *
+  * Return: Always (Success)
+  */
+int main(void)
+{
+	int n;
+
+	for (n = 0; n < 10; n++)
+	{
+	  printf("%d",n);
+	}
+
+	putchar('\n');
+
+	return (0);
+}
