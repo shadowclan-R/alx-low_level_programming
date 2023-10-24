e937b24624921bd51a240f31fac79ee5bcec0f39
shadowclan-R <rayanabdulazizm.ali@gmail.com>
2023-10-23 17:55:17 -0700

    updated

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+#include <stddef.h>
+
+/**
+* print_listint - prints all elements of a listint_t list.
+* @h: pointer to the head of the listint_t.
+* Return: lengh of the list.
+*/
+
+size_t print_listint(const listint_t *h)
+{
+	size_t listLength;
+
+	listLength = 0;
+	while (h)
+	{
+		printf("%d\n", h->n);
+		h = h->next;
+		listLength++;
+	}
+
+	return (listLength);
+}
